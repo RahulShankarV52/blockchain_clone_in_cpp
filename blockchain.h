9819b31d27b8081de09eb95e21e13dd1d9a70a53
RahulShankarV52 <rahulshankarv7@gmail.com>
2025-11-29 10:19:49 +0530

    BlockChain created Phase 1 group1

---
+++ blockchain.h
@@ -0,0 +1,26 @@
+#ifdef BLOCKCHAIN_H
+#define BLOCKCHAIN_H
+
+#include "Block.h"
+#include <vector>
+
+class BlockChain{
+  std::vector<Block> _chain;
+
+  public:
+    BlockChain(){
+      _chain.emplace_back(Block(0,"Genesis Block","0000"));
+    }
+
+    void AddBlock(Block B){
+      std::string prev_hash=_chain.back()._data;
+      B._index=static_cast<uint32_t>(_chain.size());
+      B._prevHash=prev_hash;
+      _chain.push_back(B);
+    }
+    
+    const std::vector<Block>& getChain() const {
+        return _chain;
+    }
+};
+#endif  
